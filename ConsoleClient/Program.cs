f1e5bef61f1dd4d0ceff7ecdc7b49df2a474231f
VladZaMir <wowkaakk@yandex.ru>
2023-03-21 15:36:40 +0300

    Создали сервер

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger_Stepik
 {
@@ -6,9 +7,14 @@ namespace MyMessanger_Stepik
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAI", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"RusAI","MessageText":"Privet","TimeStamp":"2023-03-21T11:23:35.8613075Z"}
+            //RusAI < 21.03.2023 11:23:35 >: Privet
+            
         }
     }
 }
